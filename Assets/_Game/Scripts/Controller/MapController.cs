239bd007062e1ac52846ce879e8f90f306f58895
Trucnm <trucnm@adone.net>
2023-03-21 21:05:17 +0700

    update physic, update map controller

--- Assets/_Game/Scripts/Controller/MapController.cs
+++ Assets/_Game/Scripts/Controller/MapController.cs
@@ -8,9 +8,42 @@ public class MapController : MonoSingleton<MapController>
     public GameObject Boss_01, Boss_02, Boss_03, Boss_04, Boss_05;
     
     public void DisableAllObject(){
-        
+        Boss_01.SetActive(false);
+        Boss_02.SetActive(false);
+        Boss_03.SetActive(false);
+        Boss_04.SetActive(false);
+        Boss_05.SetActive(false);
+    }
+    private void ActiveBossByLevel(){
+        switch (Facade.Instance.PlayerData.CurrentLevel) {
+            case 1:
+                Boss_01.SetActive(true);
+                Boss_01.transform.position = new Vector3(2.65f, -3.14f,0);
+                break;
+            case 2:
+                Boss_02.SetActive(true);
+                Boss_02.transform.position = new Vector3(2.65f, -3.14f,0);
+                break;
+            case 3:
+                Boss_03.SetActive(true);
+                Boss_03.transform.position = new Vector3(2.65f, -3.14f,0);
+                break;
+            case 4:
+                Boss_04.SetActive(true);
+                Boss_04.transform.position = new Vector3(2.65f, -3.14f,0);
+                break;
+            case 5:
+                Boss_05.SetActive(true);
+                Boss_05.transform.position = new Vector3(2.65f, -3.14f,0);
+                break;
+            default :
+                
+                break;
+        }
     }
     public void OnCreateMap(){
+        DisableAllObject();
         this.gameObject.SetActive(true);
+        ActiveBossByLevel();
     }
 }
