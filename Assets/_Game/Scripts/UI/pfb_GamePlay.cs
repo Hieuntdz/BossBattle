e1c4b964855bfabf1cf1d8f45f7e824c79bf134f
Trucnm <trucnm@adone.net>
2023-03-21 21:40:15 +0700

    update purchaser

--- Assets/_Game/Scripts/UI/pfb_GamePlay.cs
+++ Assets/_Game/Scripts/UI/pfb_GamePlay.cs
@@ -1,8 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class pfb_GamePlay : UIBehavior
 {
     public HealthBar HealthBar;
+    public Button btnBack;
+    private void Start() {
+        btnBack.onClick.AddListener(OnClickBack);
+    }
+
+    private void OnClickBack(){
+        MapController.Instance.gameObject.SetActive(false);
+        UIManager.Instance.pfb_Home.ActivePopup(true);
+    }
 }
