6f40731514cd63a18c929a69993e2cf69357dfd0
Trucnm <trucnm@adone.net>
2023-03-23 11:47:57 +0700

    update bg music, update background, update ui shop

--- Assets/_Game/Scripts/Controller/MapController.cs
+++ Assets/_Game/Scripts/Controller/MapController.cs
@@ -8,6 +8,7 @@ public class MapController : MonoSingleton<MapController>
     public PlayerMovement PlayerMovement;
     public PrefabWeapon PrefabWeapon;
     public List<Boss> ListBoss = new List<Boss>();
+    public List<GameObject> ListBG = new List<GameObject>();
     protected override void Awake()
     {
         base.Awake();
@@ -22,12 +23,12 @@ public class MapController : MonoSingleton<MapController>
             var random = UnityEngine.Random.Range(0, ListBoss.Count);
             ListBoss[random].gameObject.SetActive(true);
             ListBoss[random].transform.position = new Vector3(4.2f, -4.02f,0);
-            ListBoss[random].GetComponent<BossHealth>().health = 500;
+            ListBoss[random].GetComponent<BossHealth>().health = 500 + 100 * PlayerData.Instance.CurrentLevel;
             UIManager.Instance.pfb_GamePlay.HealthBar.SetBossHealth(ListBoss[random].GetComponent<BossHealth>());
         }else{
             ListBoss[Facade.Instance.PlayerData.CurrentLevel-1].gameObject.SetActive(true);
             ListBoss[Facade.Instance.PlayerData.CurrentLevel-1].transform.position = new Vector3(4.2f, -4.02f,0);
-            ListBoss[Facade.Instance.PlayerData.CurrentLevel-1].GetComponent<BossHealth>().health = 500;
+            ListBoss[Facade.Instance.PlayerData.CurrentLevel-1].GetComponent<BossHealth>().health = 500 + 100 * PlayerData.Instance.CurrentLevel;
             UIManager.Instance.pfb_GamePlay.HealthBar.SetBossHealth(ListBoss[Facade.Instance.PlayerData.CurrentLevel-1].GetComponent<BossHealth>());
         }
     }
@@ -40,5 +41,11 @@ public class MapController : MonoSingleton<MapController>
         Player.transform.position = new Vector3(-3.64f, -2.48f, 0);
         Player.gameObject.SetActive(true);
         UIManager.Instance.pfb_GamePlay.PlayerHealthBarController.DoAnimHealth();
+        OnCreateBackground();
+    }
+
+    private void OnCreateBackground(){
+        ListBG.ForEach(x => x.SetActive(false));
+        ListBG[UnityEngine.Random.Range(0, ListBG.Count)].SetActive(true);
     }
 }
